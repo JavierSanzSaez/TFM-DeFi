0870c8b7f909346640934d35a216792c9f7917f1
Javier Sanz Sáez <javiersse@gmail.com>
2022-05-07 17:37:57 +0200

    Hardhat is dead. Long live Truffle

--- contracts/EuriCoin.sol
+++ contracts/EuriCoin.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.26;
+pragma solidity ^0.8.0;
  
 //Safe Math Interface
  
@@ -29,9 +29,9 @@ contract SafeMath {
 //ERC Token Standard #20 Interface
  
 contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function totalSupply() public view returns (uint);
+    function balanceOf(address tokenOwner) public view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
     function transfer(address to, uint tokens) public returns (bool success);
     function approve(address spender, uint tokens) public returns (bool success);
     function transferFrom(address from, address to, uint tokens) public returns (bool success);
@@ -67,11 +67,11 @@ contract EuriCoin is ERC20Interface, SafeMath {
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
  
-    function totalSupply() public constant returns (uint) {
+    function totalSupply() public view returns (uint) {
         return _totalSupply  - balances[address(0)];
     }
  
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+    function balanceOf(address tokenOwner) public view returns (uint balance) {
         return balances[tokenOwner];
     }
  
@@ -96,7 +96,7 @@ contract EuriCoin is ERC20Interface, SafeMath {
         return true;
     }
  
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
         return allowed[tokenOwner][spender];
     }
  
