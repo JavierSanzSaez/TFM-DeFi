e0a03e3e3d4f618d16f7569e67428ce87308cf5a
Javier Sanz Sáez <javiersse@gmail.com>
2022-05-07 18:03:19 +0200

    Deploy EuriCoin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+const EuriCoin = artifacts.require("EuriCoin");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(EuriCoin);
 };
