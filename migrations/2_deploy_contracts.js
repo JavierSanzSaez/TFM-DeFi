542a897a98c1b755da3624aa195c956d16eb0a58
Javier Sanz Sáez <javiersse@gmail.com>
2022-05-14 22:14:03 +0200

    Overriding master contract after every migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ const FactoryContract = artifacts.require("FactoryContract")
 
 
 module.exports = async function(deployer,network, accounts) {
-  await deployer.deploy(MasterContract, {from: accounts[0]})
+  await deployer.deploy(MasterContract, {from: accounts[0],  overwrite: true})
   await deployer.deploy(FactoryContract, {from: accounts[0]})
   await deployer.deploy(VaultContract, {from: accounts[0]})
   await deployer.deploy(StorageContract, {from: accounts[0]})
