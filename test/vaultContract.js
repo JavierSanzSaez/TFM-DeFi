3d10df8a37a52cd0d9d4ad9ee9c75a10f008948e
Javier Sanz Sáez <javiersse@gmail.com>
2022-05-29 02:16:16 +0200

    So Truffle tests do not work correctly with different transactions....

--- test/vaultContract.js
+++ test/vaultContract.js
@@ -3,39 +3,47 @@ const EuriCoin = artifacts.require("EuriCoin")
 
 contract('Testing VaultContract', (accounts) => {
 
-  let vaultContractInstance;
+  it('should check that the masterContract is the deployer', async() => {
+    const vaultPromise = await VaultContract.deployed();
+    const vaultContractInstance = vaultPromise;
 
-  before(async () => {
-    vaultContractInstance = await VaultContract.deployed();
-    EuriCoinInstance = await EuriCoin.deployed();
-  })
-
-  it('should check that the masterContract is the deployer', async () => {
-
-    masterContract = await vaultContractInstance.masterContract.call();
-    assert.equal(masterContract, accounts[0], "masterContract (" + masterContract + ") is different than deployer (" + accounts[0] + ")");
+    const masterContract = await vaultContractInstance.masterContract.call();
+    
+    assert.equal(
+      masterContract,
+      accounts[0],
+      "masterContract (" + masterContract + ") is different than deployer (" + accounts[0] + ")");
   });
 
-  it('should register a new index with EuriCoin as the index and also as a collateral with one unit as quantity', async () => {
-    await vaultContractInstance.register_index(EuriCoinInstance.address, [EuriCoinInstance.address], [1], {from: accounts[0]});
+  it('should register a new index with EuriCoin as the index and also as a collateral with one unit as quantity', async() => {
+    const vaultPromise = await VaultContract.deployed();
+    const vaultContractInstance = vaultPromise;
 
-    let index = await vaultContractInstance.get_index.call(EuriCoinInstance.address, {from: accounts[0]});
+    const CoinPromise = await EuriCoin.deployed();
+    const CoinInstance = CoinPromise;
+    console.log("####################################################################################################")
+    console.log(CoinInstance.address)
+    console.log("####################################################################################################")
+    console.log(vaultContractInstance.address)
 
-    assert.equal(index.collateral[0], EuriCoinInstance.address, 'EuriCoin Address was not correctly registered');
+    const allowed = await CoinInstance.allowance.call(accounts[0], vaultContractInstance.address, { from: accounts[0] });
+    console.log("####################################################################################################")
+    console.log(allowed.toNumber())
+
+    await vaultContractInstance.register_index.call(CoinInstance.address, [CoinInstance.address], [1], { from: accounts[0] });
+
+    let index = await vaultContractInstance.get_index.call(CoinInstance.address, { from: accounts[0] });
+
+    assert.equal(index.collateral[0], CoinInstance.address, 'EuriCoin Address was not correctly registered');
     assert.equal(index.quantities[0], '1', 'EuriCoin Quantities was not correctly registered');
 
   });
 
   it('should mint an index', async () => {
     let index_amount = 0;
-    await vaultContractInstance.register_index(EuriCoinInstance.address, [EuriCoinInstance.address], [1], {from: accounts[0]});
-    await EuriCoinInstance.approve(vaultContractInstance.address, 9999, {from: accounts[0]})
-    .then( ()=>{
-      address = vaultContractInstance.mint_index(EuriCoinInstance.address, accounts[0], [1], {from: accounts[0]})
-       
-    }
-    )
-    index_amount= await EuriCoinInstance.balanceOf(accounts[0]);
+    await vaultContractInstance.register_index(EuriCoinInstance.address, [EuriCoinInstance.address], [1], { from: accounts[0] });
+    address = await vaultContractInstance.mint_index(EuriCoinInstance.address, accounts[0], [1], { from: accounts[0] })
+    index_amount = await EuriCoinInstance.balanceOf(accounts[0]);
     assert.equal(index_amount, 1, 'The index was not minted correctly');
 
   });
