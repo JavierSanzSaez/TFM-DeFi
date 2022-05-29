97bfabd300a9f5ac367380a7a5353317b872e60e
Javier Sanz Sáez <javiersse@gmail.com>
2022-05-29 02:14:10 +0200

    EuriCoin changed to OpenZeppelin ERC20 to avoid conflicts

--- contracts/EuriCoin.sol
+++ contracts/EuriCoin.sol
@@ -1,99 +1,10 @@
-// SPDX-License-Identifier: GPL-3.O
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.13;
- 
-//Safe Math Interface
- 
-contract SafeMath_ERC20 {
- 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
- 
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
- 
-    function safeMul(uint a, uint b) public pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
- 
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
- 
- 
-//ERC-20 Token Standard Interface
- 
-interface ERC20 {
-    function totalSupply() external view returns (uint);
-    function balanceOf(address tokenOwner) external view returns (uint balance);
-    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
-    function transfer(address to, uint tokens) external returns (bool success);
-    function approve(address spender, uint tokens) external returns (bool success);
-    function transferFrom(address from, address to, uint tokens) external returns (bool success);
- 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
- 
-//Actual token contract
- 
-contract EuriCoin is ERC20, SafeMath_ERC20 {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
-    uint public _totalSupply;
- 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
- 
-    constructor(address vaultContract){
-        symbol = "EURI";
-        name = "EuriCoin";
-        decimals = 18;
-        _totalSupply = 100000;
-        balances[vaultContract] = _totalSupply;
-    }
- 
-    function totalSupply() public view returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
- 
-    function balanceOf(address tokenOwner) public view returns (uint balance) {
-        return balances[tokenOwner];
-    }
- 
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
- 
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
- 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
- 
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
- 
-    receive() external payable {
-        revert();
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract EuriCoin is ERC20 {
+    constructor() ERC20("EuriCoin", "EURI") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
     }
 }
--- migrations/3_deploy_euricoin.js
+++ migrations/3_deploy_euricoin.js
@@ -1,11 +1,7 @@
-const VaultContract = artifacts.require("VaultContract")
 const EuriCoin = artifacts.require("EuriCoin")
 
 module.exports = async function(deployer,network, accounts) {
-    vaultInstance = await VaultContract.deployed();
-    console.log(vaultInstance.address)
-
-    await deployer.deploy(EuriCoin, vaultInstance.address,{from: accounts[0], overwrite: true} )
+    await deployer.deploy(EuriCoin, {from: accounts[0], overwrite: true} )
 };
 
 
