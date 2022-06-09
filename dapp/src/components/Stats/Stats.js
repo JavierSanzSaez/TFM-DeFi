8027854e22f7fa1b54c7ecb75103dcda4226a3b3
Javier Sanz Sáez <javiersse@gmail.com>
2022-06-09 23:34:28 +0200

    Stats page

--- dapp/src/components/Stats/Stats.js
+++ dapp/src/components/Stats/Stats.js
@@ -1,5 +1,22 @@
-const Stats = ()=>{
+import { drizzleReactHooks } from '@drizzle/react-plugin'
 
+const { useDrizzle } = drizzleReactHooks;
+
+const Stats = () => {
+
+    const { useCacheCall } = useDrizzle();
+    const masterContract = useCacheCall("StorageContract", "masterContract") || "None";
+    const vaultContract = useCacheCall("StorageContract", "vaultContract") || "None";
+    const factoryContract = useCacheCall("StorageContract", "factoryContract") || "None";
+
+    return (
+        <div className="stats">
+            <h1>Addresses of the current contracts</h1>
+            <p>Master Contract: {masterContract}</p>
+            <p>Factory Contract: {factoryContract}</p>
+            <p>Vault Contract: {vaultContract}</p>
+        </div>
+    );
 }
 
 export default Stats;
