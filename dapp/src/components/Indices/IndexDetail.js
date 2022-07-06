f1b5d98600c4e01c1ec62be39002bc26b208520e
Javier Sanz Sáez <javiersse@gmail.com>
2022-07-07 00:12:50 +0200

    Added IndexDetail :D

--- dapp/src/components/Indices/IndexDetail.js
+++ dapp/src/components/Indices/IndexDetail.js
@@ -1,5 +1,59 @@
+import { useParams } from "react-router-dom";
+import { drizzleReactHooks } from '@drizzle/react-plugin'
 
-const IndexDetail = ({}) => {
+const { useDrizzle } = drizzleReactHooks;
+
+const IndexDetail = ({ }) => {
+    let { addr } = useParams();
+    const { useCacheCall, drizzle } = useDrizzle();
+    let details = useCacheCall([addr], call => {
+        let name = call(addr, "name") || "";
+        let symbol = call(addr, "name") || "";
+        let details_dict = {
+            name: name,
+            symbol: symbol,
+            address: addr
+        }
+        return details_dict
+    })
+
+    let output = useCacheCall(['StorageContract'], call => {
+        let output = []
+        let result = call('StorageContract', 'get_collateral', details.address) || {_collateral: [],_quantities:[]}
+        for (let element = 0; element < result._collateral.length; element++) {
+            output.push(
+                <tr>
+                    <td>{result._collateral[element]}</td>
+                    <td>{parseFloat(result._quantities[element]*10**-18).toFixed(5)}</td>
+                </tr>
+            )
+        }
+        return output
+    })
+
+    return (
+        <div className="index-detail">
+            <div className="index-detail-title">
+                <h2>{details.name} ({details.symbol})</h2>
+            </div>
+            <div className="index-detail.body">
+                <p>Contract Address: {details.address}</p>
+                <div className="collateral">
+                    <p>Collateral</p>
+                    <table>
+                        <tr>
+                            <th>Token</th>
+                            <th>Quantity</th>
+                        </tr>
+                        {output}
+                    </table>
+
+                </div>
+
+            </div>
+        </div>
+    )
+
 }
 
 export default IndexDetail;
